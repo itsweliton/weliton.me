f8626c57379f8a803bd78e299ab33d0e1b5eb280
welitonderesende <welitonderesende@gmail.com>
2019-04-08 15:31:36 -0300

    Update site descrption

--- src/components/seo.js
+++ src/components/seo.js
@@ -24,7 +24,7 @@ function SEO({ description, lang, meta, keywords, title }) {
     `
   )
 
-  const metaDescription = `My personal blog, where I write stuff about technology`
+  const metaDescription = `My personal blog, where I write about technology`
 
   return (
     <Helmet
