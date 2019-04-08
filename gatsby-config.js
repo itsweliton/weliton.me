a651b4217979b660ed0bb9904dee17bcc56757e5
welitonderesende <welitonderesende@gmail.com>
2019-04-08 18:51:30 -0300

    Insert google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-117762567-2",
       },
     },
     `gatsby-plugin-feed`,
