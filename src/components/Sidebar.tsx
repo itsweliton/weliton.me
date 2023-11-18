ceded26c32e7b97abb6b4a86eca3a7f2cef687da
Weliton de Resende <welitonderesende@gmail.com>
2023-11-18 14:15:30 +0100

    adds useLocation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "gatsby";
 import "../styles/sidebar.scss";
+import { useLocation } from "@reach/router";
 
 const Sidebar: React.FC = () => {
+  const location = useLocation();
   return (
     <nav className="sidebar">
       <div className="header">
