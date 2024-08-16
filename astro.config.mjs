631c98eab719ac08f6f83dd4aced01a8bdf3f35a
Weliton Soranzo <welitonderesende@gmail.com>
2024-08-15 20:11:45 -0400

    removing serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-  }),
+  integrations: [tailwind()], 
 });
