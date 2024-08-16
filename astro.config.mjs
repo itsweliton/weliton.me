978565299bd75ec12f0b91816cface9614039952
Weliton Soranzo <welitonderesende@gmail.com>
2024-08-15 20:06:25 -0400

    trying fix build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/edge'
 
 
 // https://astro.build/config
