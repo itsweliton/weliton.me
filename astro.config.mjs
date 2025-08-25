312c7925c6953dab23ea3fc4d3fe345afc02e0dc
Weliton Soranzo <welitonderesende@gmail.com>
2025-08-25 17:43:13 +0100

    upgrade astro, add new colors, fix issues, add transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+  // Enable View Transitions API
+  viewTransitions: true,
 });
