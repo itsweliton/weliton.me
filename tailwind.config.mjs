ca7d95958564708c1529ba569b4badb1999f54a0
Weliton Soranzo <welitonderesende@gmail.com>
2025-08-25 16:46:05 +0100

    add new font titles, change color for texts, adds antialiasing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,35 +1,41 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
       screens: {
-        'mobile': {'max': '768px'},
-        'desktop': {'min': '769px'},
+        mobile: { max: '768px' },
+        desktop: { min: '769px' },
       },
-			backgroundImage: {
+      backgroundImage: {
         'bullet-journal': 'radial-gradient(circle, #2c2c2c 1px, transparent 1px)',
       },
       backgroundSize: {
-        'dots': '1.5rem 1.5rem',
+        dots: '1.5rem 1.5rem',
       },
     },
-		
-		colors: {
-			...colors,
-			gray: {
-				900: '#121212',
-				800: '#1a1a1a',
-				100: '#888888',
-				400: 'rgb(156 163 175)'
-			},
-		},
-		fontFamily: {
-			sans: ['Inter', 'sans-serif'],
-			mono: ['DM Mono', 'monospace'],
-		},
-	},
-	plugins: [require('@tailwindcss/typography')],
-}
+
+    colors: {
+      ...colors,
+      gray: {
+        900: '#121212',
+        800: '#1a1a1a',
+        100: '#888888',
+        400: 'rgb(156 163 175)',
+      },
+      'nav-gray': 'color(display-p3 0.706 0.706 0.706)',
+      'text-gray': 'color(display-p3 0.706 0.706 0.706)',
+    },
+    textColor: {
+      description: 'color(display-p3 0.706 0.706 0.706)',
+      'slate-200': 'color(display-p3 0.484 0.484 0.484)',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      mono: ['DM Mono', 'monospace'],
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
